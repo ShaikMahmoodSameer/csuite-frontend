73a13d5556f337937868beb8611e2a718863dd8c
Mahmood Sameer Shaik <69497327+ShaikMahmoodSameer@users.noreply.github.com>
2024-04-30 12:13:13 +0530

    hc3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center", color: "red" }}>Home Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat velit! Consequuntur quaerat fugit aliquam odit, temporibus rerum est repellendus pariatur, dolore, consectetur maxime error.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center", color: "red" }}>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat velit! Consequuntur quaerat fugit aliquam odit, temporibus rerum est repellendus pariatur, dolore, consectetur maxime error.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{textAlign: "center", color: "red"}}> Hello World!! </h1>
-      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat velit! Consequuntur quaerat fugit aliquam odit, temporibus rerum est repellendus pariatur, dolore, consectetur maxime error. </p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
