fbc274dd3b3da3cef61fb2459469869f4ecb525d
Mahmood Sameer Shaik <69497327+ShaikMahmoodSameer@users.noreply.github.com>
2024-04-30 11:56:04 +0530

    hc1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Hello World!! </h1>
+      <h1 style={{textAlign: "center", color: "red"}}> Hello World!! </h1>
       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat velit! Consequuntur quaerat fugit aliquam odit, temporibus rerum est repellendus pariatur, dolore, consectetur maxime error. </p>
     </div>
   );
