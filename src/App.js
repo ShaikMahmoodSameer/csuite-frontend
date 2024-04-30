96450e553ff7d5e582c0420154ce7581965690e3
Mahmood Sameer Shaik <69497327+ShaikMahmoodSameer@users.noreply.github.com>
2024-04-30 11:16:17 +0530

    c3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!!
+      <h1> Hello World!! </h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat velit! Consequuntur quaerat fugit aliquam odit, temporibus rerum est repellendus pariatur, dolore, consectetur maxime error. </p>
     </div>
   );
 }
